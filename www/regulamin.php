97dad583df98bc2c5dc038fc881af5af84772c29
marcheewa <jademm@gazeta.pl>
2017-05-17 23:20:06 +0200

    Update regulamin.php
    Zmiana regulaminu, żeby pasował :)

--- www/regulamin.php
+++ www/regulamin.php
@@ -1,83 +1,79 @@
-﻿<HTML>
+<HTML>
   <HEAD>
     <TITLE>Regulamin</TITLE>
     <META http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
     <STYLE type="text/css">
     BODY {
-    background : rgb(215,251,226);
-    text-align : center;
+    background : rgb(255,250,200);
+    text-align : justify;
     margin     : 50px;
 	}
 
 	DIV {
     color      : rgb(0,0,0);
-    background : rgb(0,128,255);
+    background : rgb(255,255,155);
     width      : auto;
-    border     : 3px black solid;
+    text-align : center;
+	border     : 1px black solid;
 	}
 
 	H1 {
     margin     : 30px;
-    border     : 1px solid black;
-    background : rgb(255,255,155);
+    text-align : center;
+	background : rgb(255,255,155);
 	}
+	
 	H2 {
     margin     : 15px;
-    border     : 1px solid black;
+	text-align : left;
 	background : rgb(255,255,155);
 	}
 
     </STYLE>
   </HEAD>
 <BODY>
-<DIV><H1>Regulamin korzystania z systemu ewidencji sprzętu IT</H1></DIV>
-<p><h2>§1</h2>
-<p align="left">Ilekroć w treści niniejszego Regulaminu lub w umowach o korzystanie z internetowego systemu
-rezerwacyjnego euroTICKET On-Line pojawiają się następujące słowa i oznaczenia, należy rozumieć je
-jak podano poniżej:</br>
-1. „Operator” oznacza firmę ITSOFT spółka z ograniczoną odpowiedzialnością spółka komandytowa.</br>
-2. „System” oznacza system euroTICKET On-Line, którego właścicielem i administratorem jest
-Operator.</br>
-3. „Moduł” oznacza wydzieloną część Systemu dotyczącą jednego typu usług Oferenta.</br>
-4. „Moduł „Autokary”” oznacza Moduł dotyczący sprzedaży biletów komunikacji autokarowej przez
-Użytkownika (z wyjątkiem stron WWW).</br>
-5. „Moduł „Ubezpieczenia”” oznacza Moduł dotyczący sprzedaży polis turystycznych przez Użytkownika
-(z wyjątkiem stron WWW).</br>
-6. „Moduł „WWW”” oznacza Moduł dotyczący sprzedaży usług Oferenta przez strony WWW.</br>
-7. „Moduł „Karty”” oznacza Moduł dotyczący sprzedaży kart przez Użytkownika (z wyjątkiem stron
-WWW).</br>
-8. „Użytkownik” oznacza firmę, która na podstawie umowy podpisanej z Operatorem ma prawo do
-korzystania z usług Systemu.</br>
-9. „Oferent” oznacza firmę, która na mocy umowy zawartej z Operatorem sprzedaje swoje usługi za
-pośrednictwem Systemu. Oferent jest jednocześnie Użytkownikiem.</br>
-10. ”Agent” oznacza firmę, która pośredniczy w sprzedaży usług za pomocą Systemu, na podstawie
-umów zawartych z Oferentami. Agent jest jednocześnie Użytkownikiem.</br>
-11.”Oddział” oznacza konto w Systemie, którego właścicielem jest Agent, przeznaczone do sprzedaży
-usług poprzez System w innej lokalizacji niż konto Agenta. Oddział jest jednocześnie Użytkownikiem.
-12. „Umowa” oznacza umowę o świadczenie usług zawartą między Operatorem, a Użytkownikiem.</br>
-13. „Usługa” oznacza usługi świadczone przez Operatora na podstawie Umowy.</br>
-14. „Faktura” oznacza dokument księgowy będący podstawą dokonywania przez Użytkownika płatności
-za Usługę świadczoną na jego rzecz przez Operatora.</br>
-15. „Okres Rozliczeniowy” oznacza okres jednego miesiąca, liczony od 11-go danego miesiąca do 10-
-go następnego miesiąca, za który dokonywane są rozliczenia należności Agenta wobec Operatora
-z tytułu świadczonych Usług, wskazanych przez Operatora na Fakturze.</br>
-16. „Oprogramowanie Użytkownika” oznacza oprogramowanie dostarczone przez Operatora
-i zainstalowane na komputerze Użytkownika.</br>
-17. ”Trasa” oznacza połączenie z grupy miast w jednym kraju do grupy miast w tym samym lub innym
-kraju realizowane jednym autokarem, przy czym połączenia antenowe na terenie kraju Oferenta nie są
-traktowane jako osobna trasa.</br>
-<h2>§2</h2>
-<p align="left">Operator ustala w niniejszym Regulaminie zasady świadczenia usług związanych z udostępnieniem
-Systemu oraz zasady korzystania z Systemu.</br></p>
-<h2>§3</h2>
-<p align="left">Operator zapewnia korzystanie z Systemu pod warunkiem posiadania przez Użytkownika: komputera
-z zainstalowanym systemem operacyjnym z rodziny Windows (XP / VISTA / 7), Oprogramowania
-Użytkownika, dostępu do sieci INTERNET, a za jej pośrednictwem do serwera Systemu, oraz po
-spełnieniu innych wymagań technicznych koniecznych do prawidłowej pracy Systemu.</br></p>
-<h2>§4</h2>
-<p align="left">Operator świadczy usługi związane z Systemem w zakresie i na zasadach określonych w Umowie.
-</p></p>
-<a href="rejestracja.php"><H1>Powrót do rejestracji</H1></a></p>
+<H1>Regulamin korzystania z programu IT Inwentaryzator</H1>
+<p><h2>1. Postanowienia ogólne i definicje</h2>
+<p align="left">Nazwy użyte w Regulaminie należy rozumieć następująco:</br></p>
+<li><b>IT Inwentaryzator</b> – program komputerowy umożliwiający zarządzanie informacjami o sprzęcie i oprogramowaniu przypisanych do pracownika.</li>
+<li><b>Administrator Systemu</b> – osoba odpowiedzialna za nadzorowanie stosowania środków technicznych i organizacyjnych zapewniających ochronę przetwarzanych danych, w tym w szczególności za przeciwdziałanie dostępowi osób trzecich do systemu za podejmowanie odpowiednich działań w przypadku wykrycia naruszeń bezpieczeństwa tych danych.</li>
+<li><b>Dane osobowe</b> – wszelkie informacje dotyczące osoby fizycznej. Osobą możliwą do zidentyfikowania jest osoba, której tożsamość można określić bezpośrednio lub pośrednio, w szczególności przez powołanie się na numer identyfikacyjny, imię lub nazwisko.</li>
+<li><b>Dane o sprzęcie i oprogramowaniu</b> – wszelkie informacje dotyczące sprzętu teleinformatycznego i oprogramowania znajdującego się w zasobach firmy.</li>
+<li><b>Użytkownik Systemu</b> – osoba upoważniona do bezpośredniego dostępu do danych przetwarzanych w systemie.</li>
+<li><b>Przetwarzanie danych</b> - jakiekolwiek operacje wykonywane na danych, takie jak zbieranie, utrwalanie, przechowywanie, opracowywanie, zmienianie, udostępnianie i usuwanie.</li>
+<li><b>Konto></b> – zbiór uprawnień nadawanych użytkownika systemu w celu podglądu lub modyfikacji przechowywanych danych.</li>
+<li><b>Login</b> – formalna nazwa konta nadawana podczas procesu rejestracji.</li>
+<li><b>Hasło</b> – ciąg znaków zdefiniowany przez Użytkownika Systemu zgodny z zasadami tworzenia haseł służący zalogowaniu się do systemu.</li>
+
+<p><h2>2. Uzyskanie dostępu do systemu IT Inwentaryzator</h2>
+<p align="left">1. W celu uzyskania konta w systemie użytkownik zobowiązany jest do wypełnienia formularza a następnie przesłania go drogą administracyjną do administratora systemu</p>
+<p align="left">2. Użytkownik zobowiązany jest do wyboru loginu oraz ustalenia bezpiecznego hasła dostępu</p>
+<p align="left">3. Poprzez bezpieczne hasło dostępu rozumie się hasło zawierające co najmniej jedną cyfrę oraz znak specjalny, nie dające się zbyt łatwo odgadnąć przez osoby trzecie</p>
+<p align="left">4. W przypadku zapomnienia hasła użytkownik zobowiązany jest do jak najszybszego kontaktu z administratorem systemu.</p>
+<p align="left">5. Przed oddaleniem się od komputera użytkownik powinien upewnić się, że ekran został zablokowany</p>
+<p align="left">6. Dostęp do aplikacji może zostać uchylony na prośbę użytkownika lub przy rozwiązywaniu z nim stosunku pracy.</p>
+<p align="left">Zabronione jest podejmowanie prób przejęcia przez użytkownika wyższych uprawnień niż te nadane przez administratora.</p>
+
+<p><h2>3. Poufność i bezpieczeństwo danych</h2>
+<p align="left">1. Użytkownik systemu zobowiązuje się do zachowania poufności względem pozyskiwanych i wprowadzanych danych oraz do nieudostępniania ich podmiotom trzecim.</p>
+<p align="left">2. Użytkownikowi przyznawane są minimalne możliwe uprawnienia dostępu pozwalające na korzystanie z systemu zgodnie z zakresem pełnionych obowiązków.</p>
+<p align="left">3. Zabrania się przechowywania haseł w ogólnodostępnych miejscach oraz udostępniania hasł osobom nie związanym z obsługą systemu.</p>
+<p align="left">4. Administrator zobowiązuje się do cyklicznego sporządzania kopii zapasowej bazy danych firmowego sprzętu komputerowego oraz oprogramowania celem zapewnienia większej niezawodności działania systemu.</p>
+<p align="left">5. Administrator prowadzi monitorowania i tworzenia dzienników zdarzeń w celu umożliwienia rejestracji działań związanych z bezpieczeństwem przetwarzania danych osobowych.</p>
+
+<p><h2>4. Warunki korzystania z systemu</h2>
+<p align="left">1. System IT Inwentaryzator dostepny jest tylko i wyłącznie dla zarejestrowanych użytkowników.</p>
+<p align="left">2. Korzystanie z systemu możliwe jest jedynie w siedzibie organizacji, wyłącnie na zaaprobowanym sprzęcie komputerowym.</p>
+<p align="left">3. Administrator systemu zobowiązuje się do informowania użytkowników drogą elektroniczną o planowanej konserwacji lub o awariach systemu</p>
+
+
+<p><h2>5. Postanowienia końcowe</h2>
+<p align="left">1. Dostęp do systemu zagwarantowany jest w zakresie i na zasadach określonych w Regulaminie.
+</p>
+<p align="left">2. Niniejsze postanowienia użytkownik akceptuje poprzez zaznaczenie "Akceptuj regulamin" na formularzu rejestracyjnym oraz przez wysłanie formularza</p>
+<p align="left">3. Zastrzega się prawo do zmiany niniejszego regulaminu</p>
+
+<a href="rejestracja.php"><H1>Powrót</H1></a></p>
 
 </BODY>
 </HTML>
